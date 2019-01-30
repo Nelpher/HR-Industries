ad511da405705ffab79b51c6c58fa792769e4c79
ludovic.gendre <ludovic.gendre@g-mail.fr>
2019-01-30 09:26:44 -0500

    moins le contener

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,7 +2,6 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
     if message.text.include? "Bonjour"
