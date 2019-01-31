9e6fcd36b2dc1c907aba26eebd67639ce2f1bc6f
ludovic.gendre <ludovic.gendre@g-mail.fr>
2019-01-31 07:11:06 -0500

    test3

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,6 +1,10 @@
 require 'facebook/messenger'
 
 include Facebook::Messenger
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: access_token,
+  subscribed_fields: %w[ACCESS_TOKEN]
+)
 
 
 Bot.on :message do |message|
